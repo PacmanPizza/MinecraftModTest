50199e2d79d34845ea3df3f8b6d2c1da59984b21
David Jose <pineapplez475@gmail.com>
2020-03-10 20:26:20 +1100

    Basic mod initialisation added

---
+++ src/main/java/com/pacmanpizza/testmod/Reference.java
@@ -0,0 +1,8 @@
+package com.pacmanpizza.testmod;
+
+public class Reference {
+	public static final String MODID = "testmod";
+	public static final String MODNAME = "PacmanPizza's Test Mod";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+}
